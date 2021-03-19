581c8e629a56b458f2d00580a12341307085b635
Ken Chung <ken.chung@thebaselab.com>
2021-03-19 20:51:04 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+
+// Setup
+const app = express();
+const port = 3003;
+const config = require('./webpack.config.dev.js');
+const compiler = webpack(config);
+const middleware = webpackMiddleware(compiler, {
+  publicPath: "/",
+  serverSideRender: true,
+  watchOptions: {
+    ignored: /.*/
+  }
+});
+app.use(middleware);
+app.get('/', (req, res) => {
+  res.sendFile('public/index.html', { root: __dirname });
+});
+
+app.listen(port, () => {
+  console.log(
+    'Launching app... http://localhost:' + port + '\n'
+  );
+});
